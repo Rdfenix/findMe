d3f3e0063e0c251ec0531a0b05113b1dcfb9c79a
Rudnei Carlos de Oliveira <rhoodsk8erboy@gmail.com>
2017-11-28 16:08:18 -0200

    Update app.js

--- public/js/angular/app.js
+++ public/js/angular/app.js
@@ -3,9 +3,6 @@ angular.module('findMe', ['ngRoute'])
 
     $routeProvider.when('/', {
         templateUrl: 'views/main.html'
-    }).when('/login', {
-        templateUrl: 'views/login.html',
-        controller: 'LoginWithFacebook as loginCtrl'
     }).when('/search', {
         templateUrl: 'views/search.html'
     }).when('/events/:name', {
@@ -14,20 +11,3 @@ angular.module('findMe', ['ngRoute'])
     });
     $routeProvider.otherwise({redirectTo: '/'});
 });
-
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '500208297017698',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v2.11'
-    });
-    FB.AppEvents.logPageView();    
-};
-(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11&appId=500208297017698';
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
